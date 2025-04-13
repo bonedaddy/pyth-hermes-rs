ecb9b9c1f67008cfece88de5f73cdfd981c10410
bonedaddy <alex@bonedaddy.io>
2025-04-12 22:56:26 -0700

    add helper function to convert price into a floating point

--- crates/client/src/types.rs
+++ crates/client/src/types.rs
@@ -86,3 +86,34 @@ pub struct ParsedPriceUpdate {
     pub ema_price: RpcPrice,
     pub metadata: RpcPriceFeedMetadata,
 }
+
+impl RpcPrice {
+    /// Converts the pyth reported price from an integer into a floating point
+    pub fn to_f64(&self) -> Option<f64> {
+        let price = self.price.parse::<u64>().ok()?;
+        Some(price as f64 / ((10_u64.pow(self.expo.unsigned_abs())) as f64))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::RpcPrice;
+
+    #[test]
+    fn test_rpc_price_to_f64() {
+        let price = RpcPrice {
+            price: "12971500000".to_string(),
+            conf: "6486733".to_string(),
+            expo: -8,
+            publish_time: 1744523548,
+        };
+        assert_eq!(price.to_f64().unwrap(), 129.715);
+        let price = RpcPrice {
+            price: "160644665033".to_string(),
+            conf: "73725033".to_string(),
+            expo: -8,
+            publish_time: 1744523627,
+        };
+        assert_eq!(price.to_f64().unwrap(), 1606.44665033)
+    }
+}
